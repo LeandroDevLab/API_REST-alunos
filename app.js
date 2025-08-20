94a77aba134f09fbfbd36628804fc96bb6d4be42
LeandroDevLab <prof.oliota@gmail.com>
2025-08-19 22:44:30 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// comentário para desabilitar o erro na linha debaixo
+// eslint-disable-next-line
+const teste = 22;
+const teste2 = 22; // eslint-disable-line
+
+export default () => {
+  console.log('Olá mundo!');
+};
